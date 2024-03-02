8300f412a3b56f8a17476e8b7efc08380e0f299f
Esben Rask Bach <erb18@student.aau.dk>
2024-03-02 16:08:23 +0100

    Initial commit

---
+++ src/main/java/com/esben/bordtennis/bordtennis/BordtennisApplication.java
@@ -0,0 +1,13 @@
+package com.esben.bordtennis.bordtennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BordtennisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BordtennisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esben/bordtennis/bordtennis/BordtennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esben.bordtennis.bordtennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BordtennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
