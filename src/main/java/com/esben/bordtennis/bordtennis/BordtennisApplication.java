c4dd90febf8e574cfdb90fa9cb8e4aa300fca84a
Esben Rask Bach <erb18@student.aau.dk>
2024-03-03 19:44:17 +0100

    Add hello endpoint

--- src/main/java/com/esben/bordtennis/bordtennis/BordtennisApplication.java
+++ src/main/java/com/esben/bordtennis/bordtennis/BordtennisApplication.java
@@ -2,9 +2,13 @@ package com.esben.bordtennis.bordtennis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 @SpringBootApplication
-public class BordtennisApplication {
+public class BordtennisApplication extends Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BordtennisApplication.class, args);
--- src/main/java/com/esben/bordtennis/bordtennis/user/boundary/UserResource.java
+++ src/main/java/com/esben/bordtennis/bordtennis/user/boundary/UserResource.java
@@ -4,19 +4,32 @@ import com.esben.bordtennis.bordtennis.user.CreateUserRequestDTO;
 import com.esben.bordtennis.bordtennis.user.control.UserController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping(path = "/users")
-@Component
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("users")
+@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+@Consumes(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+@Controller
 public class UserResource {
 
     @Autowired
     private UserController userController;
 
-    @PostMapping(value = "/create", consumes = "")
-    public void create(@RequestBody CreateUserRequestDTO createUserRequestDTO) {
+    @POST
+    @Path("create")
+    public Response create(@RequestBody CreateUserRequestDTO createUserRequestDTO) {
+        //userController.create(createUserRequestDTO);
+        return Response.accepted().build();
+    }
 
+    @GET
+    @Path("hello")
+    public String greet() {
+        return "hej";
     }
 }
