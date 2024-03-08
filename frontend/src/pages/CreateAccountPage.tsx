8d54763cc9d040b5193631dca2feb79d79d902eb
bernardperru <47976460+bernardperru@users.noreply.github.com>
2024-03-08 15:06:53 +0100

    Update CreateAccountPage.tsx
    Added a useState to contain error messages for invalid input, so that they can be rendered

--- frontend/src/pages/CreateAccountPage.tsx
+++ frontend/src/pages/CreateAccountPage.tsx
@@ -8,20 +8,36 @@ import {
 const inputcss =
   "appearance-none border border-gray-300 py-2 px-4 my-2 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent";
 
+type InvalidInput = {
+  email: string;
+  name: string;
+  password: string;
+  passwordMatch: string;
+};
+
 export function CreateAccountPage() {
+  const [invalidInput, setInvalidInput] = useState<InvalidInput>({
+    email: "",
+    name: "",
+    password: "",
+    passwordMatch: "",
+  });
   const navigate = useNavigate();
+
+  const emailValidation = new RegExp(
+    "/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$/"
+  );
+
+  const passwordValidation = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})"
+  );
+
+  const nameValidation = new RegExp("/^[A-Za-z\u00C0-\u017Fs'-]+$/");
+
   const validateAccount = (accountInfo: CreateAccountForm) => {
-    //const c = new RegExp("^.{8,32}$");
-
-    const emailValidation = new RegExp(
-      "/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$/"
-    );
-
-    const passwordValidation = new RegExp(
-      "^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})"
-    );
-
-    const nameValidation = new RegExp("/^[A-Za-z\u00C0-\u017Fs'-]+$/");
+    if (!emailValidation.test(accountInfo.email)) {
+      console.log("Email is not valid");
+    }
 
     if (!nameValidation.test(accountInfo.name)) {
       console.log("name is not valid");
@@ -33,10 +49,6 @@ export function CreateAccountPage() {
 
     if (!(accountInfo.password === accountInfo.secondPassword)) {
       console.log("Passwords do not match");
-    }
-
-    if (!emailValidation.test(accountInfo.email)) {
-      console.log("Email is not valid");
     }
   };
 
