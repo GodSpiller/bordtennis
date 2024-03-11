40c207d2e137c7b587dc2011ac0c7e7535c74ec7
bernardperru <47976460+bernardperru@users.noreply.github.com>
2024-03-11 11:33:00 +0100

    Implemented create account form using react-hook-form

--- frontend/src/components/login/CreateAccount.tsx
+++ frontend/src/components/login/CreateAccount.tsx
@@ -1,181 +1,92 @@
-import { useState } from "react";
-import { InvalidInput } from "../../pages/CreateAccountPage";
+import { SubmitHandler, useForm } from "react-hook-form";
 const inputcss =
   "appearance-none border border-gray-300 py-2 px-4 my-2 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent";
 
-export type CreateAccountForm = {
+type FormFields = {
   email: string;
-  password: string;
-  secondPassword: string;
   name: string;
-};
-
-type AccountFormError = {
-  email: boolean;
-  password: boolean;
-  secondPassword: boolean;
-  name: boolean;
+  password: string;
+  password2: string;
 };
 
 export function CreateAccount() {
-  const [formState, setFormState] = useState<CreateAccountForm>({
-    email: "",
-    password: "",
-    secondPassword: "",
-    name: "",
-  });
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormFields>();
 
-  const [error, setErrors] = useState<AccountFormError>({
-    email: false,
-    password: false,
-    secondPassword: false,
-    name: false,
-  });
-
-  function validate() {
-    if (
-      valid("email" as keyof CreateAccountForm) &&
-      valid("name" as keyof CreateAccountForm) &&
-      valid("password" as keyof CreateAccountForm) &&
-      valid("secondPassword" as keyof CreateAccountForm)
-    ) {
-      console.log("inputs are valid");
-    } else {
-      console.log("not valid");
-    }
-  }
-
-  function valid(key: keyof CreateAccountForm) {
-    switch (key) {
-      case "email": {
-        const emailValidation = new RegExp(
-          /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/
-        );
-        if (!emailValidation.test(formState.email)) {
-          console.log("email not valid");
-          setErrors({ ...error, email: true });
-          return false;
-        }
-        setErrors({ ...error, email: false });
-        break;
-      }
-      case "name": {
-        const nameValidation = new RegExp(
-          /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/
-        );
-        if (!nameValidation.test(formState.name)) {
-          console.log("name not valid");
-          setErrors({ ...error, name: true });
-          return false;
-        }
-        setErrors({ ...error, name: false });
-        break;
-      }
-      case "password": {
-        const passwordValidation = new RegExp(
-          /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/
-        );
-        if (!passwordValidation.test(formState.password)) {
-          console.log("password not valid");
-          setErrors({ ...error, password: true });
-          return false;
-        }
-        setErrors({ ...error, password: false });
-        break;
-      }
-      case "secondPassword": {
-        if (formState.password !== formState.secondPassword) {
-          console.log("passwords do not match valid");
-          setErrors({ ...error, secondPassword: true });
-          return false;
-        }
-        setErrors({ ...error, secondPassword: false });
-        break;
-      }
-    }
-    return true;
-  }
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data);
+  };
 
   return (
-    <div className="flex flex-col w-80">
-      <form className="flex flex-col">
-        <div>
-          <label htmlFor="email">Email</label>
-        </div>
-        <input
-          className={inputcss}
-          id="email"
-          name="email"
-          type="email"
-          value={formState.email}
-          onChange={(e) =>
-            setFormState({
-              ...formState,
-              email: e.target.value,
-            })
-          }
-        ></input>
-      </form>
-      <form className="flex flex-col">
-        <label htmlFor="name">Name</label>
-        <input
-          className={inputcss}
-          id="name"
-          name="name"
-          type="text"
-          value={formState.name}
-          onChange={(e) =>
-            setFormState({
-              ...formState,
-              name: e.target.value,
-            })
-          }
-        ></input>
-      </form>
-      <form className="flex flex-col">
-        <label htmlFor="password1">Password</label>
-        <input
-          className={inputcss}
-          autoComplete=""
-          id="password1"
-          name="password1"
-          type="password"
-          required={true}
-          minLength={8}
-          value={formState.password}
-          onChange={(e) =>
-            setFormState({
-              ...formState,
-              password: e.target.value,
-            })
-          }
-        ></input>
-      </form>
-      <form className="flex flex-col">
-        <label htmlFor="password2">Repeat password</label>
-        <input
-          className={inputcss}
-          autoComplete=""
-          id="password2"
-          name="password2"
-          type="password"
-          required={true}
-          minLength={8}
-          value={formState.secondPassword}
-          onChange={(e) =>
-            setFormState({
-              ...formState,
-              secondPassword: e.target.value,
-            })
-          }
-        ></input>
-      </form>
-      <button
-        className="border border-grey hover:border-black rounded px-1"
-        onClick={() => validate()}
-      >
-        Create Account
-      </button>
-    </div>
+    <form className="flex flex-col w-80" onSubmit={handleSubmit(onSubmit)}>
+      <input
+        className={inputcss}
+        {...register("email", {
+          required: "Email is required",
+          pattern: {
+            value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/,
+            message: "Invalid email",
+          },
+        })}
+        type="text"
+        placeholder="Email"
+      />
+      {errors.email && (
+        <div className="text-red-500">{errors.email.message}</div>
+      )}
+      <input
+        className={inputcss}
+        {...register("name", {
+          required: "Name is required",
+          pattern: {
+            value: /^[A-Za-z'-]+(?:\s+[A-Za-z'-]+)*$/,
+            message: "Name contained illegal characters",
+          },
+        })}
+        type="text"
+        placeholder="Name"
+      />
+      {errors.name && <div className="text-red-500">{errors.name.message}</div>}
+      <input
+        className={inputcss}
+        {...register("password", {
+          required: "Password required",
+
+          pattern: {
+            value:
+              /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/,
+            message:
+              "Password must contain at least one lowercase, uppercase, and one special character and must be at least 8 characters",
+          },
+          minLength: 8,
+        })}
+        type="password"
+        placeholder="Password"
+      />
+      {errors.password && (
+        <div className="text-red-500">{errors.password.message}</div>
+      )}
+      <input
+        className={inputcss}
+        {...register("password2", {
+          required: "Password do not match",
+          validate: (val: string) => {
+            if (watch("password") !== val) {
+              return "Your passwords do not match";
+            }
+          },
+        })}
+        type="password"
+        placeholder="Repeat Password"
+      />
+      {errors.password2 && (
+        <div className="text-red-500">{errors.password2.message}</div>
+      )}
+      <button type="submit">Opret Konto</button>
+    </form>
   );
 }
--- frontend/src/pages/CreateAccountPage.tsx
+++ frontend/src/pages/CreateAccountPage.tsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  CreateAccount,
-  CreateAccountForm,
-} from "../components/login/CreateAccount";
+import { CreateAccount } from "../components/login/CreateAccount";
 
 const inputcss =
   "appearance-none border border-gray-300 py-2 px-4 my-2 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent";
